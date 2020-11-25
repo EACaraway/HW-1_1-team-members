dfd9cc1f1505dd1902471127b41430dbb9b0803a
Evan Caraway <evan.caraway@outlook.com>
2020-11-24 22:18:20 -0800

    Project files

---
+++ HomeWorkWeek1Problem1 team members/HomeWorkWeek1Problem1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkWeek1Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Group 1 Team members.
+            Console.WriteLine("Srinivas Reddy Doma (Sri), Kipack Jeong, and Evan Caraway");
+        }
+    }
+}
